561db0d72c046b00d435b1503d34f241d53c6224
Tobias25 <96679895+GamerTobi@users.noreply.github.com>
2024-04-19 10:08:17 +0000

    springfunctie

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const KEY_w = 87;
 const KEY_a = 65;
 const KEY_s = 83;
 const KEY_d = 68;
-var spelStatus = UITLEG;
+var spelStatus = 1;
 
 var spelerRichting = 0; // 0 is omhoog, 45 is rechts, 90 is omlaag, 135 is links
 var speler2Richting = 0; // 0 is omhoog, 45 is rechts, 90 is omlaag, 135 is links
@@ -34,28 +34,33 @@ var spelerY2 = 0; // y-positie van speler2
 var health = 0;  // health van speler
 var snelheid = 0;  // snelheid van speler
 
+var spelerSpringt = false; // of speler aan het springen is
+var springSnelheid = 2; // hoe snel speler beweegt tijdens springen
+var springSnelheidStart = 4; // snelheid bij starten van springen
+var zwaartekracht = 0.2; // snelheid van de zwaartekracht
+
 /* ********************************************* */
 /* functies die je gebruikt in je game           */
 /* ********************************************* */
-function reset(){
+function reset() {
 
-   spelerRichting = 0; // 0 is omhoog, 45 is rechts, 90 is omlaag, 135 is links
-   speler2Richting = 0; // 0 is omhoog, 45 is rechts, 90 is omlaag, 135 is links
-   spelerX = 600; // x-positie van speler1
-   spelerY = 600; // y-positie van speler1
-   spelerX2 = 800; // x-positie van speler2
-   spelerY2 = 600; // y-positie van speler2
-   health = 200;  // health van speler
-   snelheid = 4;  // snelheid van speler
+  spelerRichting = 0; // 0 is omhoog, 45 is rechts, 90 is omlaag, 135 is links
+  speler2Richting = 0; // 0 is omhoog, 45 is rechts, 90 is omlaag, 135 is links
+  spelerX = 600; // x-positie van speler1
+  spelerY = 600; // y-positie van speler1
+  spelerX2 = 800; // x-positie van speler2
+  spelerY2 = 600; // y-positie van speler2
+  health = 200;  // health van speler
+  snelheid = 4;  // snelheid van speler
 }
 /**
  * Updatet globale variabelen met posities van speler, vijanden en kogels
  */
 var beweegAlles = function() {
   // speler
-   if (keyIsDown(LEFT_ARROW)) {
+  if (keyIsDown(LEFT_ARROW)) {
     spelerX = spelerX - snelheid;
-     spelerRichting = 135;
+    spelerRichting = 135;
   }
   if (keyIsDown(RIGHT_ARROW)) {
     spelerX = spelerX + snelheid;
@@ -69,27 +74,32 @@ var beweegAlles = function() {
     spelerY = spelerY + snelheid;
     spelerRichting = 90;
   }
-  
+
+  if (keyIsDown(32)) {
+    spelerSpringt = true;
+    springSnelheid = springSnelheidStart;
+  }
+  if (spelerSpringt === true) {
+
+    spelerY = spelerY - springSnelheid;
+    springSnelheid = springSnelheid - zwaartekracht;
+    if (spelerY > 600) {
+      spelerSpringt = false;
+
+    }
+  }
   // vijand
-  if (keyIsDown(KEY_a)) {
+  while (spelStatus === SPELEN) {
     spelerX2 = spelerX2 - snelheid;
-    speler2Richting = 135;
-  }
-  if (keyIsDown(KEY_d)) {
-    spelerX2 = spelerX2 + snelheid;
-    speler2Richting = 45;
-  }
-  if (keyIsDown(KEY_w)) {
-    spelerY2 = spelerY2 - snelheid;
     speler2Richting = 0;
-  }
-  if (keyIsDown(KEY_s)) {
-    spelerY2 = spelerY2 + snelheid;
-    speler2Richting = 90;
+    if (spelerX2 > 1200) {
+      spelerX2 = -100;
+    }
+
   }
   // kogel
-};
 
+}
 /**
  * Checkt botsingen
  * Verwijdert neergeschoten dingen
@@ -97,10 +107,10 @@ var beweegAlles = function() {
  */
 var verwerkBotsing = function() {
   // botsing speler tegen vijand
-if (spelerX - spelerX2 < 50 && spelerX - spelerX2 > -50 ) {
-console.log("botsing");
-  health = health - 200;
-}
+  if (spelerX - spelerX2 < 50 && spelerX - spelerX2 > -50) {
+    console.log("botsing");
+    health = health - 200;
+  }
   // botsing kogel tegen vijandw
 
   // update punten en health
@@ -118,29 +128,29 @@ var tekenAlles = function() {
   if (speler2Richting === 45 || speler2Richting === 135) {
     fill("red");
     rect(spelerX2 - 50, spelerY2 - 25, 100, 50);
-    }
-    if (speler2Richting === 0 || speler2Richting === 90) {
-      fill("red");
-      rect(spelerX2 - 25, spelerY2 - 50, 50, 100);
-      }
+  }
+  if (speler2Richting === 0 || speler2Richting === 90) {
+    fill("red");
+    rect(spelerX2 - 25, spelerY2 - 50, 50, 100);
+  }
   fill("white");
   ellipse(spelerX2, spelerY2, 10, 10);
   // kogel
 
   // speler
   if (spelerRichting === 45 || spelerRichting === 135) {
-  fill("white");
-  rect(spelerX - 50, spelerY - 25, 100, 50);
+    fill("white");
+    rect(spelerX - 50, spelerY - 25, 100, 50);
   }
   if (spelerRichting === 0 || spelerRichting === 90) {
     fill("white");
     rect(spelerX - 25, spelerY - 50, 50, 100);
-    }
-  
+  }
+
   fill("red");
   ellipse(spelerX, spelerY, 10, 10);
-  
-  
+
+
   // punten en health
 
 };
@@ -177,7 +187,7 @@ function draw() {
       spelStatus = GAMEOVER;
     }
   }
-  if ( keyIsDown(ESCAPE)) {
+  if (keyIsDown(ESCAPE)) {
     spelStatus = PAUZE;
   };
   if (spelStatus === GAMEOVER) {
@@ -185,12 +195,12 @@ function draw() {
     console.log("GAME OVER");
     textSize(50);
     fill("white");
-    text("GAME OVER" , 280, 150);
-    text("Druk op enter om naar startscherm te gaan" , 280, 200);
+    text("GAME OVER", 280, 150);
+    text("Druk op enter om naar startscherm te gaan", 280, 200);
     // teken game-over scherm
-    if ( keyIsDown(ENTER)) {
+    if (keyIsDown(ENTER)) {
       spelStatus = UITLEG;
-      
+
     };
   }
   if (spelStatus === PAUZE) {
@@ -198,20 +208,20 @@ function draw() {
     console.log("PAUZE");
     textSize(50);
     fill("white");
-    text("Dit is het pauze menu." , 280, 150);
-    text("Kies wat u wilt doen;" , 280, 200);
+    text("Dit is het pauze menu.", 280, 150);
+    text("Kies wat u wilt doen;", 280, 200);
     fill("red");
     rect(250, 300, 400, 200);
     fill("black");
-    text("PLAY" , 375, 420);
+    text("PLAY", 375, 420);
     fill("red");
     rect(700, 300, 400, 200);
     fill("black");
-    text("RESET" , 825, 420);
+    text("RESET", 825, 420);
     // teken pauze scherm
     if (mouseIsPressed && mouseY > 300 && mouseY < 500 && mouseX > 250 && mouseX < 650) {
       spelStatus = SPELEN;
-    } 
+    }
     if (mouseIsPressed && mouseY > 300 && mouseY < 500 && mouseX > 700 && mouseX < 1100) {
       spelStatus = SPELEN;
       reset();
@@ -222,12 +232,12 @@ function draw() {
     console.log("uitleg");
     textSize(50);
     fill("white");
-    text("Welkom bij de game," , 280, 150);
-    text("Druk op de knop om verder te gaan!" , 280, 200);
+    text("Welkom bij de game,", 280, 150);
+    text("Druk op de knop om verder te gaan!", 280, 200);
     fill("red");
     rect(450, 300, 400, 200);
-  fill("black");
-    text("PLAY" , 575, 420);
+    fill("black");
+    text("PLAY", 575, 420);
     // teken uitleg scherm
     if (mouseIsPressed && mouseY > 300 && mouseY < 500 && mouseX > 450 && mouseX < 850) {
       spelStatus = SPELEN;
